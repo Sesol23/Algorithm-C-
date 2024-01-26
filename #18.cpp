e82a31dba86fc7052d1569e40c7ba3740f1dafb8
Sesol23 <154987679+Sesol23@users.noreply.github.com>
2024-01-26 19:13:03 +0900

    층간소음

---
+++ #18.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int N, M;
+	scanf("%d %d", &N, &M);
+	
+	int noise;
+	int cnt = 0;
+	int max_cnt = -1;
+	for(int i = 0; i < N; i++) {
+		scanf("%d", &noise);
+		if(noise > M) cnt++;
+		else cnt = 0;
+		if(max_cnt < cnt) max_cnt = cnt;
+	}
+	
+	if(max_cnt == 0) printf("-1\n");
+	else printf("%d\n", max_cnt);
+	
+	return 0;
+}
